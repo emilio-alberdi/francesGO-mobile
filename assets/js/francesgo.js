c392ec9226d907ce7a2f1012857fd6d99ee03d82
Gustavo Felipe Garcia <gustavofelipegarcia@gmail.com>
2012-07-31 10:58:46 -0300

    Changed Service Location to Test

--- assets/js/francesgo.js
+++ assets/js/francesgo.js
@@ -4,7 +4,7 @@
 
 	//$("[id^=appFooter]").empty().append($('#footerNav'));
 	
-   baseUrl = 'http://192.168.1.111:8080/francesGo2-portal/mobile/';
+   baseUrl = 'http://190.193.220.146:8080/francesGo2-portal/mobile/';
    
    $( document ).bind( "mobileinit", function() {
 	    // Make your jQuery Mobile framework configuration changes here!
