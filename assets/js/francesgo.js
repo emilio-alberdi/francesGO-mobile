3da2d94e071aae081dc801ecba3ef994e15dd247
darioguzman <dario.guzman@artdecode.com.ar>
2012-11-21 18:13:32 -0300

    url local

--- assets/js/francesgo.js
+++ assets/js/francesgo.js
@@ -6,7 +6,7 @@
 
 	
    	baseUrl = '';
-   	baseUrl = 'http://192.168.1.101:8080/francesGo2-portal/mobile/';
+   	baseUrl = 'http://192.168.1.106:8080/francesGo2-portal/mobile/';
 //   	baseUrl = 'https://bbvawebqa.bancofrances.com.ar/francesGo2-portal/mobile/';
 //   	baseUrl = 'http://m.francesgo.com.ar/francesGo2-Portal/mobile/';
    	$( document ).bind( "mobileinit", function() {
