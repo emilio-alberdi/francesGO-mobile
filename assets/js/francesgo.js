2ee7d5d562f3f8cf8243b69fd899386ed65586e8
darioguzman <dario.guzman@artdecode.com.ar>
2013-05-06 13:04:10 -0300

    Update francesgo.js

--- assets/js/francesgo.js
+++ assets/js/francesgo.js
@@ -1,8 +1,8 @@
 	//$("[id^=appFooter]").empty().append($('#footerNav'));
 
 
-   	baseUrl = 'http://192.168.1.107:8080/francesGo2-portal/mobile/';
-//   	baseUrl = 'https://bbvawebqa.bancofrances.com.ar/francesGo2-portal/mobile/';
+ //  	baseUrl = 'http://192.168.1.107:8080/francesGo2-portal/mobile/';
+  	baseUrl = 'https://bbvawebqa.bancofrances.com.ar/francesGo2-portal/mobile/';
 //   	baseUrl = 'http://m.francesgo.com.ar/francesGo2-Portal/mobile/';
    	$( document ).bind( "mobileinit", function() {
 	    // Make your jQuery Mobile framework configuration changes here!
